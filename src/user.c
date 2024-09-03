6c861cdb5e0e445e62cfe92a402eb0a772a6aa91
Jester-2-6 <33711287+Jester-2-6@users.noreply.github.com>
2024-09-03 16:08:53 +0000

    EOD 09/03

--- src/user.c
+++ src/user.c
@@ -1,1 +1,11 @@
 #include "graph.h"
+
+void duplicateCircuit(NODE* graph, int num) {
+	int node_pointer_old, node_pointer_new;
+
+	// TODO: Implement this function
+}
+
+void readBench(NODE* graph, int num) {
+	// TODO: Implement this function
+}
