c75295eaa185fce98a965c98e2750073f30bc326
Jester-2-6 <33711287+Jester-2-6@users.noreply.github.com>
2024-09-22 16:16:02 -0500

    Implement circuit duplicator and comparator (#3)

--- src/main.c
+++ src/main.c
@@ -2,53 +2,51 @@
 /***************************************************************************************************************************
  Main Function(Serial and Parallel Fault Simulation)
 ****************************************************************************************************************************/
-void main(int argc,char **argv)
+void main(int argc, char** argv)
 {
-FILE *fisc,*fvec,*ffau,*fres;             //file pointers used for .isc file, .vec file, .faults file and resultfile
-int Max,Opt,Npi,Npo,Total,Tfs;              //maxnode id,option,tot no of PIs,tot no of Pos,Tot no of input patterns& faults in.vec in.faults
+    FILE* fisc, * fvec, * ffau, * fres;             //file pointers used for .isc file, .vec file, .faults file and resultfile
+    int Max, Opt, Npi, Npo, Total, Tfs;              //maxnode id,option,tot no of PIs,tot no of Pos,Tot no of input patterns& faults in.vec in.faults
 
-NODE graph[Mnod];                         //structure used to store the ckt information in .isc file 
-//PATTERN vector[Mpt];                      //structure used to store the input vectors information in .vec file 
+    NODE graph[Mnod], new_graph[Mnod];                         //structure used to store the ckt information in .isc file 
+    //PATTERN vector[Mpt];                      //structure used to store the input vectors information in .vec file 
 
-//FAULT stuck[Mft];                      //structure used to store the faults information in .faults file
-int a,b,c,d;                             //random variables
-
-
-//Read the .isc file and store the information in graph structure
-// fisc=fopen(argv[1],"r");                           //file pointer to open .isc file 
-// Max=0; 
-// Max=ReadIsc(fisc,graph);                 //read .isc file and return index of last node in graph formed
-// fclose(fisc);                            //close file pointer for .isc file
-// PrintCircuit(graph,Max);                 //print all members of graph structure
-
-// Read the .bench file and store the information in graph structure
-fisc = fopen(argv[1], "r");                           //file pointer to open .bench file
-Max = readBench(graph, fisc);                //read .isc file and return index of last node in graph formed
-fclose(fisc);                            //close file pointer for .isc file
-PrintCircuit(graph, Max);                 //print all members of graph structure
-
-FILE *fbenchOut;
-fbenchOut = fopen(argv[2], "w");                           //file pointer to open .bench file
-writeBench(graph, fbenchOut, Max);                //read .isc file and return index of last node in graph formed
-
-//Read the .vec file and store the information in  vector structure
+    //FAULT stuck[Mft];                      //structure used to store the faults information in .faults file
+    int a, b, c, d;                             //random variables
 
 
+    //Read the .isc file and store the information in graph structure
+    // fisc=fopen(argv[1],"r");                           //file pointer to open .isc file 
+    // Max=0; 
+    // Max=ReadIsc(fisc,graph);                 //read .isc file and return index of last node in graph formed
+    // fclose(fisc);                            //close file pointer for .isc file
+    // PrintCircuit(graph,Max);                 //print all members of graph structure
 
+    // Read the .bench file and store the information in graph structure
+    fisc = fopen(argv[1], "r");                           //file pointer to open .bench file
+    Max = readBench(graph, fisc);                //read .isc file and return index of last node in graph formed
+    fclose(fisc);                            //close file pointer for .isc file
 
- 
+    PrintCircuit(graph, Max);                 //print all members of graph structure
 
-//Opt=0; 
-//Opt=atoi(argv[3]);                          //getting the option from terminal for xval
-//fres=fopen(argv[2],"w");                           //file pointer to open .out file for printing results
+    Max = duplicateCircuit(graph, new_graph, Max);
+    FILE* fbenchOut;
+    fbenchOut = fopen(argv[2], "w");                           //file pointer to open .bench file
+    PrintCircuit(new_graph, Max);                 //print all members of graph structure
+    writeBench(new_graph, fbenchOut, Max);                //read .isc file and return index of last node in graph formed
 
-//Perform Logic Simulation for each Input vector and print the Pos .val in output file   
+    //Read the .vec file and store the information in  vector structure
 
-//fclose(fres);                                                  //close file pointer for .out file
+    //Opt=0; 
+    //Opt=atoi(argv[3]);                          //getting the option from terminal for xval
+    //fres=fopen(argv[2],"w");                           //file pointer to open .out file for printing results
 
-ClearCircuit(graph,Mnod);                                      //clear memeory for all members of graph
-//for(a=0;a<Total;a++){ bzero(vector[a].piv,Mpi); }                //clear memeory for all members of vector
-return;
+    //Perform Logic Simulation for each Input vector and print the Pos .val in output file   
+
+    //fclose(fres);                                                  //close file pointer for .out file
+
+    ClearCircuit(graph, Mnod);                                      //clear memeory for all members of graph
+    //for(a=0;a<Total;a++){ bzero(vector[a].piv,Mpi); }                //clear memeory for all members of vector
+    return;
 }//end of main
 /****************************************************************************************************************************/
 
--- src/user.c
+++ src/user.c
@@ -1,9 +1,149 @@
 #include "graph.h"
 
-void duplicateCircuit(NODE* graph, int num) {
-	int node_pointer_old, node_pointer_new;
+int duplicateCircuit(NODE* graph, NODE* new_graph, int tot) {
+	int node_pointer_old, node_pointer_new, new_1, new_2;
+	LIST* fin;
+	LIST* fot;
 
-	// TODO: Implement this function
+	node_pointer_new = 0;
+
+	for (int i = 0; i < Mnod; i++) {
+		InitializeCircuit(new_graph, i);
+	}
+
+	for (node_pointer_old = 0; node_pointer_old <= tot; node_pointer_old++) {
+		if (node_pointer_new < node_pointer_old) node_pointer_new = node_pointer_old;
+
+		if (graph[node_pointer_old].Type == 0) {
+			continue;
+		} else if (graph[node_pointer_old].Type == INPT) {
+			new_graph[node_pointer_old].Type = INPT;
+		} else {
+			copyNode(&new_graph[node_pointer_new], &graph[node_pointer_old], node_pointer_old);
+			node_pointer_new++;
+			copyNode(&new_graph[node_pointer_new], &graph[node_pointer_old], node_pointer_old);
+			node_pointer_new++;
+		}
+	}
+
+	for (int i = 0; i <= tot; i++) {
+		if (graph[i].Type == INPT) {
+			fot = graph[i].Fot;
+
+			while (fot != NULL) {
+				if (graph[fot->id].Type > INPT) {
+					InsertList(&new_graph[i].Fot, mapNewtoOld(new_graph, fot->id, node_pointer_new, 0));
+					InsertList(&new_graph[i].Fot, mapNewtoOld(new_graph, fot->id, node_pointer_new, 1));
+				}
+
+				new_graph[i].Nfo++;
+				new_graph[i].Nfo++;
+				fot = fot->next;
+			}
+
+		} else if (graph[i].Type > INPT) {
+			new_1 = mapNewtoOld(new_graph, i, node_pointer_new, 0);
+			new_2 = mapNewtoOld(new_graph, i, node_pointer_new, 1);
+
+			fin = graph[i].Fin;
+
+			while (fin != NULL) {
+				if (graph[fin->id].Type == INPT) {
+					InsertList(&new_graph[new_1].Fin, fin->id);
+					InsertList(&new_graph[new_2].Fin, fin->id);
+				} else if (graph[fin->id].Type > INPT) {
+					InsertList(&new_graph[new_1].Fin, mapNewtoOld(new_graph, fin->id, node_pointer_new, 0));
+					InsertList(&new_graph[new_2].Fin, mapNewtoOld(new_graph, fin->id, node_pointer_new, 1));
+				}
+
+				fin = fin->next;
+			}
+
+			fot = graph[i].Fot;
+
+			while (fot != NULL) {
+				if (graph[fot->id].Type > INPT) {
+					InsertList(&new_graph[new_1].Fot, mapNewtoOld(new_graph, fot->id, node_pointer_new, 0));
+					InsertList(&new_graph[new_2].Fot, mapNewtoOld(new_graph, fot->id, node_pointer_new, 1));
+				}
+
+				fot = fot->next;
+			}
+		}
+	}
+	printf("Total: %d\n", node_pointer_new);
+	node_pointer_new = insertComparator(new_graph, node_pointer_new);
+	printf("Total: %d\n", node_pointer_new);
+	return node_pointer_new;
+}
+
+int insertComparator(NODE* graph, int tot) {
+	int i = 0;
+	int new_node_pointer = tot + 1;
+	LIST* or_fin;
+
+	or_fin = NULL;
+
+	while (i <= tot) {
+		if (graph[i].Po > 0) {
+			// No need to initialize since we initialized MNod nodes in the previous function
+			InsertList(&graph[new_node_pointer].Fin, i);
+			InsertList(&graph[new_node_pointer].Fin, i + 1);
+			InsertList(&graph[i].Fot, new_node_pointer);
+			InsertList(&graph[i + 1].Fot, new_node_pointer);
+			InsertList(&or_fin, new_node_pointer);
+
+			graph[new_node_pointer].Type = XOR;
+			graph[new_node_pointer].Nfi = 2;
+			graph[new_node_pointer].Nfo = 1;
+
+			graph[i].Po = 0;
+			graph[i].Nfo++;
+			graph[i + 1].Po = 0;
+			graph[i + 1].Nfo++;
+
+			new_node_pointer++;
+			i += 2;
+		} else {
+			i++;
+		}
+	}
+
+	graph[new_node_pointer].Type = OR;
+	graph[new_node_pointer].Po = 1;
+
+	while (or_fin != NULL) {
+		InsertList(&graph[new_node_pointer].Fin, or_fin->id);
+		InsertList(&graph[or_fin->id].Fot, new_node_pointer);
+		graph[new_node_pointer].Nfi++;
+		or_fin = or_fin->next;
+	}
+
+	return new_node_pointer;
+}
+
+int mapNewtoOld(NODE* graph, int new_id, int limit, int skip) {
+	for (int i = 0; i <= limit; i++) {
+		if (graph[i].Mark == new_id) {
+			if (skip > 0) {
+				skip--;
+			} else {
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
+
+void copyNode(NODE* dest, NODE* src, int src_id) {
+	dest->Type = src->Type;
+	dest->Po = src->Po;
+	dest->Nfi = src->Nfi;
+	dest->Nfo = src->Nfo;
+	dest->Mark = src_id;
+	dest->Cval = src->Cval;
+	dest->Fval = src->Fval;
 }
 
 int readBench(NODE* graph, FILE* bench) {
@@ -11,15 +151,16 @@ int readBench(NODE* graph, FILE* bench) {
 	char line[IscLineLen];
 	int line_id = 0;
 	int total = 0;
-	int *tot = &total;
+	int* tot = &total;
 	int node_id = 0;
 	int i;
 
-	for(i=0;i<Mnod;i++) { 
-		InitializeCircuit(graph,i); 
-	} 
+	for (i = 0;i < Mnod;i++) {
+		InitializeCircuit(graph, i);
+	}
 
 	while (fgets(line, sizeof(line), bench)) {
+		printf("%s", line);
 		LineToGate(line, graph, &node_id, tot);
 	}
 	printf("Total: %d\n", *tot);
@@ -28,7 +169,7 @@ int readBench(NODE* graph, FILE* bench) {
 	return *tot;
 }
 
-void LineToGate(char *line, NODE *Node, int *node_id_ptr, int *tot) {
+void LineToGate(char* line, NODE* Node, int* node_id_ptr, int* tot) {
 	int nid_tmp;
 	// skip comment lines
 	if (line[0] == '\n' || line[0] == '#') {
@@ -48,19 +189,19 @@ void LineToGate(char *line, NODE *Node, int *node_id_ptr, int *tot) {
 	} else {
 		// Handle gates
 		int fout = extractFout(line);
-		char *name = extractName(line);
-		char *fin = extractParenthesis(line);
+		char* name = extractName(line);
+		char* fin = extractParenthesis(line);
 
 		if (fout > *tot) *tot = fout;
-		
+
 		// Assign the type of the gate
 		Node[fout].Type = AssignType(name);
 
 		// Assign the fanin list
-		char *token = strtok(fin, ",");
+		char* token = strtok(fin, ",");
 		while (token != NULL) {
 			int fin_id = atoi(token);
-			
+
 			InsertList(&Node[fout].Fin, fin_id);
 			InsertList(&Node[fin_id].Fot, fout);
 			Node[fout].Nfi++;
@@ -71,13 +212,13 @@ void LineToGate(char *line, NODE *Node, int *node_id_ptr, int *tot) {
 	}
 }//end of LineToGate
 
-char *extractParenthesis(char *line) {
-	char *start = strchr(line, '(');
-	char *end = strchr(line, ')');
+char* extractParenthesis(char* line) {
+	char* start = strchr(line, '(');
+	char* end = strchr(line, ')');
 
 	if (start != NULL && end != NULL && end > start) {
 		size_t length = end - start - 1;
-		char *result = (char *)malloc(length + 1);
+		char* result = (char*)malloc(length + 1);
 		if (result != NULL) {
 			strncpy(result, start + 1, length);
 			result[length] = '\0';
@@ -88,16 +229,16 @@ char *extractParenthesis(char *line) {
 	return NULL;
 }//end of ExtractParenthesis
 
-char *extractName(char *line) {
-	char *start = strchr(line, '=');
-	char *end = strchr(line, '(');
+char* extractName(char* line) {
+	char* start = strchr(line, '=');
+	char* end = strchr(line, '(');
 
 	if (start != NULL && end != NULL && end > start) {
 		start++; // Move past the '=' character
 		while (*start == ' ') start++; // Trim leading whitespaces
 
 		size_t length = end - start;
-		char *result = (char *)malloc(length + 1);
+		char* result = (char*)malloc(length + 1);
 		if (result != NULL) {
 			strncpy(result, start, length);
 			result[length] = '\0';
@@ -108,12 +249,12 @@ char *extractName(char *line) {
 	return NULL;
 }//end of ExtractName
 
-int extractFout(char *line) {
-	char *start = line;
-	char *end = strchr(line, ' ');
+int extractFout(char* line) {
+	char* start = line;
+	char* end = strchr(line, ' ');
 
 	size_t length = end - start;
-	char *result = (char *)malloc(length + 1);
+	char* result = (char*)malloc(length + 1);
 	if (result != NULL) {
 		strncpy(result, line, length);
 		result[length] = '\0';
@@ -147,14 +288,14 @@ void writeBench(NODE* graph, FILE* bench, int max) {
 	fprintf(bench, "\n");
 
 	// Write the gates
-	char *gate_type;
+	char* gate_type;
 	for (int i = 0; i <= max; i++) {
 		if (graph[i].Type > 1) {
 			gate_type = invertType(graph[i].Type);
 
 			fprintf(bench, "%d = %s(", i, gate_type);
 
-			LIST *fin = graph[i].Fin;
+			LIST* fin = graph[i].Fin;
 			while (fin != NULL) {
 				fprintf(bench, "%d", fin->id);
 				fin = fin->next;
